b4b3e5acbe34c61e2b99f831ca66b0b1b74f1d46
Anh Quoc <chutuilavip@gmail.com>
2022-11-16 09:13:21 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/unauthorized', [AuthController::class, 'unauthorized'])->name('unauthorized');
